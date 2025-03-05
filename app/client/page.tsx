8fe7957deeaae8fc3e991c7214f8478495fc6a66
namidapoo <83203852+ve1997@users.noreply.github.com>
2025-03-05 10:04:16 +0900

    feat: wrap DataFetcher in Suspense for improved loading state

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -1,16 +1,18 @@
-import type { FC } from 'react';
-import { DataFetcher } from './data-fetcher';
-import { fetchData } from './fetch-data';
+import { Suspense, type FC } from "react";
+import { DataFetcher } from "./data-fetcher";
+import { fetchData } from "./fetch-data";
 
 const Page: FC = () => {
-    const dataPromise = fetchData();
+	const dataPromise = fetchData();
 
-    return (
-        <div>
-            <h1>Page</h1>
-            <DataFetcher dataPromise={dataPromise} />
-        </div>
-    );
-}
+	return (
+		<div className="flex h-screen items-center justify-center">
+			<h1>Page</h1>
+			<Suspense fallback={<div>loading...</div>}>
+				<DataFetcher dataPromise={dataPromise} />
+			</Suspense>
+		</div>
+	);
+};
 
 export default Page;
